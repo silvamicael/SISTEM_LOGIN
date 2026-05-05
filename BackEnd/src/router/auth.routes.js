e8a89de285a738bc5b432287859c249207f41260
silvamicael <micaelmadri@gmail.com>
2026-05-04 21:40:12 -0300

    Arrumando os Limitadores nas rotas

--- BackEnd/src/router/auth.routes.js
+++ BackEnd/src/router/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import * as c from '../controllers/auth.controller.js';
+import { limitadorGetUser } from "../config/rateLimit.js";
 
 const routerUser = Router();
 
-routerUser.post('/cadastro', c.createUser);
-routerUser.post('/login', c.login);
+routerUser.post('/cadastro', limitadorGetUser, c.createUser);
+routerUser.post('/login', limitadorGetUser, c.login);
 
 export default routerUser;
--- BackEnd/src/router/user.router.js
+++ BackEnd/src/router/user.router.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { limitadorGetUser } from "../config/rateLimit.js";
 import { authJWT, checkRole } from "../middleware/auth.middleware.js";
 import * as c from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("/perfil",limitadorGetUser, authJWT, c.getPerfil);
+router.get("/perfil", authJWT, c.getPerfil);
 router.put("/perfil", authJWT, c.updatePerfil);
 router.delete("/conta", authJWT, c.deleteConta);
 
