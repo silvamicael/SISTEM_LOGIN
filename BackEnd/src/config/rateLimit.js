3940ec21855d39858b6a17d15cb7c9ab30720885
silvamicael <micaelmadri@gmail.com>
2026-05-04 21:37:18 -0300

    Adicionando Limitador por Rotas especificas

--- BackEnd/src/config/rateLimit.js
+++ BackEnd/src/config/rateLimit.js
@@ -4,9 +4,22 @@ import rateLimit from 'express-rate-limit';
 export const limitadorGlobal = rateLimit({
     windowMs: 15*60*1000, // Janela de tempo para fazer algo
     max: 100, // Número máximo de requisições por IP
+    statusCode: 429, 
     message: {
         erro: 'Muita requisições por minuto',
     },
     standardHeaders: true, // Envia RateLimit-* nos headers
     legacyHeaders: false // Desativa o X-RateLimit-* antigo
+});
+
+// ========= Configurações Anti-Spawn =========
+export const limitadorGetUser = rateLimit({
+    windowMs: 15*60*1000,
+    max: 10,
+    statusCode: 429,
+    message: {
+        erro: 'Muita requisições por minuto',
+    },
+    standardHeaders: true,
+    legacyHeaders: false
 });
--- BackEnd/src/router/user.router.js
+++ BackEnd/src/router/user.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { limitadorGetUser } from "../config/rateLimit.js";
 import { authJWT, checkRole } from "../middleware/auth.middleware.js";
 import * as c from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("/perfil", authJWT, c.getPerfil);
+router.get("/perfil",limitadorGetUser, authJWT, c.getPerfil);
 router.put("/perfil", authJWT, c.updatePerfil);
 router.delete("/conta", authJWT, c.deleteConta);
 
